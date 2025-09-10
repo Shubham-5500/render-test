ec5601696d4bb898f981ff1dc0bdf8c4124cf232
Shubham Mishra <shubhamm10.06.2006@gmail.com>
2025-09-10 15:30:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,30 @@ def home():
 
 @app.get("/data")
 def get_data():
-    sample_data = [{
-        "city": "Delhi",
-        "temperature": 32,
-        "status": "Sunny",
-        "aqi": 120
-    },{
-        "city": "Delhi",
-        "temperature": 32,
-        "status": "Sunny",
-        "aqi": 120
-    },{
-        "city": "Delhi",
-        "temperature": 32,
-        "status": "Sunny",
-        "aqi": 120
-    },{
-        "city": "Delhi",
-        "temperature": 32,
-        "status": "Sunny",
-        "aqi": 120
-    }],
+   sample_data = [
+        {
+            "city": "Delhi",
+            "temperature": 32,
+            "status": "Sunny",
+            "aqi": 120
+        },
+        {
+            "city": "Mumbai",
+            "temperature": 30,
+            "status": "Cloudy",
+            "aqi": 140
+        },
+        {
+            "city": "Chennai",
+            "temperature": 34,
+            "status": "Humid",
+            "aqi": 110
+        },
+        {
+            "city": "Kolkata",
+            "temperature": 31,
+            "status": "Rainy",
+            "aqi": 135
+        }
+    ]
     return sample_data
